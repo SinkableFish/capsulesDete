ca8dc64e1edcb1e4b983213922ee6bf5dca548b8
SinkableFish <nihongtao96@gmail.com>
2017-04-27 20:10:12 +0800

    add main

---
+++ main.m
@@ -0,0 +1,11 @@
+image1 = imread('./data/1.tiff');
+image2 = imread('./data/2.tiff');
+image3 = imread('./data/3.tiff');
+image4 = imread('./data/4.tiff');
+image5 = imread('./data/5.tiff');
+
+
+
+% functionForImage1(image1);
+% funcitonForImage23(image2);
+functionForImage45(image5);
